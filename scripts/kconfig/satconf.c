d7a02520933e5842adb77eb626fb070025b9c6b7
Vegard Nossum <vegard.nossum@oracle.com>
2015-12-30 15:50:17 +0100

    satconfig: FOO=y in .satconfig excludes FOO=m
    For a tristate variable, FOO=y in the .satconfig should not allow FOO=m
    to be a valid assignment.
    
    Signed-off-by: Vegard Nossum <vegard.nossum@oracle.com>

--- scripts/kconfig/satconf.c
+++ scripts/kconfig/satconf.c
@@ -1182,6 +1182,8 @@ int main(int argc, char *argv[])
 					break;
 				case yes:
 					picosat_assume(sym_y(sym));
+					if (sym->type == S_TRISTATE)
+						picosat_assume(-sym_m(sym));
 					break;
 				case mod:
 					assert(sym->type == S_TRISTATE);
