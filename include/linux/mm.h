ba954b6c4c46c13b177da92bbc07547a5d875887
Alexey Gladkov <legion@kernel.org>
2021-05-24 15:38:06 +0200

    proc: Implement /proc/self/meminfo
    The /proc/meminfo contains information regardless of the cgroups
    restrictions. This file is still widely used [1]. This means that all
    these programs will not work correctly inside container [2][3][4]. Some
    programs try to respect the cgroups limits, but not all of them
    implement support for all cgroup versions [5].
    
    Correct information can be obtained from cgroups, but this requires the
    cgroups to be available inside container and the correct version of
    cgroups to be supported.
    
    There is lxcfs [6] that emulates /proc/meminfo using fuse to provide
    information regarding cgroups. This patch can help them.
    
    This patch adds /proc/self/meminfo that contains a subset of
    /proc/meminfo respecting cgroup restrictions.
    
    We cannot just create /proc/self/meminfo and make a symlink at the old
    location because this will break the existing apparmor rules [7].
    Therefore, the patch adds a separate file with the same format.
    
    [1] https://codesearch.debian.net/search?q=%2Fproc%2Fmeminfo
    [2] https://sources.debian.org/src/erlang/1:23.2.6+dfsg-1/lib/os_mon/c_src/memsup.c#L300
    [3] https://sources.debian.org/src/p7zip/16.02+dfsg-8/CPP/Windows/System.cpp/#L103
    [4] https://sources.debian.org/src/systemd/247.3-5/src/oom/oomd.c/#L138
    [5] https://sources.debian.org/src/nodejs/12.21.0%7Edfsg-4/deps/uv/src/unix/linux-core.c/#L1059
    [6] https://linuxcontainers.org/lxcfs/
    [7] https://gitlab.com/apparmor/apparmor/-/blob/master/profiles/apparmor.d/abstractions/base#L98
    
    Changelog
    ---------
    v2:
    * Show total memory available to the process.
    
    Signed-off-by: Alexey Gladkov <legion@kernel.org>

--- include/linux/mm.h
+++ include/linux/mm.h
@@ -2467,6 +2467,20 @@ static inline int early_pfn_to_nid(unsigned long pfn)
 extern int __meminit early_pfn_to_nid(unsigned long pfn);
 #endif
 
+struct meminfo {
+	struct sysinfo si;
+	unsigned long pages[NR_LRU_LISTS];
+	unsigned long cached;
+	unsigned long swapcached;
+	unsigned long anon_pages;
+	unsigned long mapped;
+	unsigned long nr_pagetable;
+	unsigned long dirty_pages;
+	unsigned long writeback_pages;
+	unsigned long slab_reclaimable;
+	unsigned long slab_unreclaimable;
+};
+
 extern void set_dma_reserve(unsigned long new_dma_reserve);
 extern void memmap_init_range(unsigned long, int, unsigned long,
 		unsigned long, unsigned long, enum meminit_context,
@@ -2477,6 +2491,7 @@ extern int __meminit init_per_zone_wmark_min(void);
 extern void mem_init(void);
 extern void __init mmap_init(void);
 extern void show_mem(unsigned int flags, nodemask_t *nodemask);
+extern long si_mem_available_mi(struct meminfo *mi);
 extern long si_mem_available(void);
 extern void si_meminfo(struct sysinfo * val);
 extern void si_meminfo_node(struct sysinfo *val, int nid);
