5ec6759ab3b617f9c12449a9606b6f0b5a7582d0
Alexey Gladkov <legion@kernel.org>
2022-05-16 12:41:23 +0200

    sysctl: API extension for handling sysctl
    This adds additional optional functions for handling open, read, and
    write operations that can be customized for each sysctl file. It also
    creates ctl_context that persists from opening to closing the file in
    the /proc/sys.
    
    The context allows us to store dynamic information at the time the file
    is opened. This eliminates the need to duplicate ctl_table in order to
    dynamically change .data, .extra1 or .extra2.
    
    This API extends the existing one and does not require any changes to
    already existing sysctl handlers.
    
    Signed-off-by: Alexey Gladkov <legion@kernel.org>

--- include/linux/sysctl.h
+++ include/linux/sysctl.h
@@ -116,9 +116,9 @@ struct ctl_table_poll {
 	wait_queue_head_t wait;
 };
 
-static inline void *proc_sys_poll_event(struct ctl_table_poll *poll)
+static inline unsigned long proc_sys_poll_event(struct ctl_table_poll *poll)
 {
-	return (void *)(unsigned long)atomic_read(&poll->event);
+	return (unsigned long)atomic_read(&poll->event);
 }
 
 #define __CTL_TABLE_POLL_INITIALIZER(name) {				\
@@ -127,6 +127,21 @@ static inline void *proc_sys_poll_event(struct ctl_table_poll *poll)
 
 #define DEFINE_CTL_TABLE_POLL(name)					\
 	struct ctl_table_poll name = __CTL_TABLE_POLL_INITIALIZER(name)
+
+struct ctl_context {
+	struct ctl_table *table;
+	unsigned long poll_event;
+	void *ctl_data;
+};
+
+struct inode;
+
+struct ctl_fops {
+	int (*open) (struct ctl_context *, struct inode *, struct file *);
+	int (*release) (struct ctl_context *, struct inode *, struct file *);
+	ssize_t (*read) (struct ctl_context *, struct file *, char *, size_t *, loff_t *);
+	ssize_t (*write) (struct ctl_context *, struct file *, char *, size_t *, loff_t *);
+};
 
 /* A sysctl table is an array of struct ctl_table: */
 struct ctl_table {
@@ -139,6 +154,7 @@ struct ctl_table {
 	struct ctl_table_poll *poll;
 	void *extra1;
 	void *extra2;
+	struct ctl_fops *ctl_fops;
 } __randomize_layout;
 
 struct ctl_node {
