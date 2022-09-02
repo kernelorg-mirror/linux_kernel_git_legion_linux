6ee4da666bcb645208914d5466d5ca4fbceb8f83
Alexey Gladkov <legion@kernel.org>
2022-09-02 11:37:07 +0200

    sysctl: fs: Replace proc_dointvec_minmax by read and write functions
    Signed-off-by: Alexey Gladkov <legion@kernel.org>

--- include/linux/writeback.h
+++ include/linux/writeback.h
@@ -357,8 +357,7 @@ extern unsigned int dirty_expire_interval;
 extern unsigned int dirtytime_expire_interval;
 extern int laptop_mode;
 
-int dirtytime_interval_handler(struct ctl_table *table, int write,
-		void *buffer, size_t *lenp, loff_t *ppos);
+extern struct ctl_fops dirtytime_interval_fops;
 
 void global_dirty_limits(unsigned long *pbackground, unsigned long *pdirty);
 unsigned long wb_calc_thresh(struct bdi_writeback *wb, unsigned long thresh);
