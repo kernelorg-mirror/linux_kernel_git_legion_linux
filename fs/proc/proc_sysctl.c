a87c5e19df0bd73055566e15cd54779e69ec00cc
Alexey Gladkov <legion@kernel.org>
2022-09-02 12:05:21 +0200

    sysctl: Drop proc_dointvec_minmax
    Signed-off-by: Alexey Gladkov <legion@kernel.org>

--- fs/proc/proc_sysctl.c
+++ fs/proc/proc_sysctl.c
@@ -1191,7 +1191,6 @@ static int sysctl_check_table(const char *path, struct ctl_table *table)
 		if ((entry->proc_handler == proc_dostring) ||
 		    (entry->proc_handler == proc_dointvec) ||
 		    (entry->ctl_fops == &sysctl_uintvec_fops) ||
-		    (entry->proc_handler == proc_dointvec_minmax) ||
 		    (entry->proc_handler == proc_dou8vec_minmax) ||
 		    (entry->proc_handler == proc_dointvec_jiffies) ||
 		    (entry->proc_handler == proc_dointvec_userhz_jiffies) ||
@@ -1364,7 +1363,7 @@ out:
  * Several default handlers are available to cover common cases -
  *
  * proc_dostring(), proc_dointvec(), proc_dointvec_jiffies(),
- * proc_dointvec_userhz_jiffies(), proc_dointvec_minmax(),
+ * proc_dointvec_userhz_jiffies(),
  * proc_doulongvec_ms_jiffies_minmax(), proc_doulongvec_minmax()
  *
  * It is the handler's job to read the input buffer from user memory
