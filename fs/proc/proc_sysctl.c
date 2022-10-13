c79794d83b61e5500c83d0b1055d821e54f84e24
Alexey Gladkov <legion@kernel.org>
2022-10-13 21:09:42 +0200

    sysctl: Drop proc_douintvec
    Signed-off-by: Alexey Gladkov <legion@kernel.org>

--- fs/proc/proc_sysctl.c
+++ fs/proc/proc_sysctl.c
@@ -1167,8 +1167,7 @@ static int sysctl_check_table_array(const char *path, struct ctl_table *table)
 {
 	int err = 0;
 
-	if ((table->proc_handler == proc_douintvec) ||
-	    (table->ctl_fops == &sysctl_uintvec_fops)) {
+	if ((table->ctl_fops == &sysctl_uintvec_fops)) {
 		if (table->maxlen != sizeof(unsigned int))
 			err |= sysctl_err(path, table, "array not allowed");
 	}
@@ -1191,7 +1190,6 @@ static int sysctl_check_table(const char *path, struct ctl_table *table)
 
 		if ((entry->proc_handler == proc_dostring) ||
 		    (entry->proc_handler == proc_dointvec) ||
-		    (entry->proc_handler == proc_douintvec) ||
 		    (entry->ctl_fops == &sysctl_uintvec_fops) ||
 		    (entry->proc_handler == proc_dointvec_minmax) ||
 		    (entry->proc_handler == proc_dou8vec_minmax) ||
