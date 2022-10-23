7fa1a17c22e70031b54af498cbf69160ec316aa9
Alexey Gladkov <legion@kernel.org>
2022-10-23 18:49:36 +0200

    sysctl: seccomp: Move audit_actions_logged upper
    Signed-off-by: Alexey Gladkov <legion@kernel.org>

--- kernel/seccomp.c
+++ kernel/seccomp.c
@@ -2270,6 +2270,38 @@ static bool seccomp_actions_logged_from_names(u32 *actions_logged, char *names)
 	return true;
 }
 
+static void audit_actions_logged(u32 actions_logged, u32 old_actions_logged,
+				 int ret)
+{
+	char names[sizeof(seccomp_actions_avail)];
+	char old_names[sizeof(seccomp_actions_avail)];
+	const char *new = names;
+	const char *old = old_names;
+
+	if (!audit_enabled)
+		return;
+
+	memset(names, 0, sizeof(names));
+	memset(old_names, 0, sizeof(old_names));
+
+	if (ret)
+		new = "?";
+	else if (!actions_logged)
+		new = "(none)";
+	else if (!seccomp_names_from_actions_logged(names, sizeof(names),
+						    actions_logged, ","))
+		new = "?";
+
+	if (!old_actions_logged)
+		old = "(none)";
+	else if (!seccomp_names_from_actions_logged(old_names,
+						    sizeof(old_names),
+						    old_actions_logged, ","))
+		old = "?";
+
+	return audit_seccomp_actions_logged(new, old, !ret);
+}
+
 static int read_actions_logged(struct ctl_table *ro_table, void *buffer,
 			       size_t *lenp, loff_t *ppos)
 {
@@ -2315,38 +2347,6 @@ static int write_actions_logged(struct ctl_table *ro_table, void *buffer,
 
 	seccomp_actions_logged = *actions_logged;
 	return 0;
-}
-
-static void audit_actions_logged(u32 actions_logged, u32 old_actions_logged,
-				 int ret)
-{
-	char names[sizeof(seccomp_actions_avail)];
-	char old_names[sizeof(seccomp_actions_avail)];
-	const char *new = names;
-	const char *old = old_names;
-
-	if (!audit_enabled)
-		return;
-
-	memset(names, 0, sizeof(names));
-	memset(old_names, 0, sizeof(old_names));
-
-	if (ret)
-		new = "?";
-	else if (!actions_logged)
-		new = "(none)";
-	else if (!seccomp_names_from_actions_logged(names, sizeof(names),
-						    actions_logged, ","))
-		new = "?";
-
-	if (!old_actions_logged)
-		old = "(none)";
-	else if (!seccomp_names_from_actions_logged(old_names,
-						    sizeof(old_names),
-						    old_actions_logged, ","))
-		old = "?";
-
-	return audit_seccomp_actions_logged(new, old, !ret);
 }
 
 static int seccomp_actions_logged_handler(struct ctl_table *ro_table, int write,
